7295444223da381676d1a200ca55a1c2a438c332
Pierre <pl@sherpa-solutions.com>
2015-03-26 16:39:18 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Should I Buy An Apple Watch ?
+A random gift, for a not random person.
+
+Available online at http://shouldibuyanapplewatch.com/
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!-- SAVE THE ALMIGHTY OTTERS -->
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="robot" content="index, nofollow">
+		<title>Shoud I Buy an Apple Watch ?</title>
+	</head>
+	<body style="text-align:center;font-size:20px;font-family:sans-serif">
+	<?php if(isset($_GET['answer']) && $_GET['answer'] == 'yes') { ?>
+		<h1 style="font-size:5em;">NO MEANS NO</h1>
+		<span>Stop trying to change the fact that it's stupid.</span>
+	<?php } else { ?>
+		<h1 style="font-size:5em;">NO</h1>
+		<span>For once in your life, respect yourself.</span>
+	<?php } ?>
+		<p style="line-height:1.5em;font-size:0.75em;margin-top:3em">If you have too much money, you can doate it to usefull organizations like:<br>
+			<a href="http://otter.org" target="_blank">The International Otter Survival Fund</a><br>
+			<a href="http://www.worldwildlife.org/" target="_blank">The World Wildlife Fund</a><br>
+		</p>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60489296-1', 'auto');
+  ga('send', 'pageview');
+</script>
+	</body>
+</html>
